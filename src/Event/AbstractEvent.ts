838167b250938d56dca7f940d5677fe7683a7313
mikeburke <mikeburke.214@gmail.com>
2024-11-24 22:07:08 +0000

    - Updated webpack version - Enabled TypeScript strict mode

--- src/Event/AbstractEvent.ts
+++ src/Event/AbstractEvent.ts
@@ -5,18 +5,21 @@ import { EventInterface } from "./EventInterface";
 export abstract class AbstractEvent<Payload extends AbstractPayload, Response> implements EventInterface<Response>
 {
   private readonly payload: Payload;
-
+  
   public constructor(payload: Payload | PublicProperties<Payload>)
   {
-    if (!payload || typeof payload !== "object" || Array.isArray(payload)) throw new TypeError(`Payload data passed to ${this.constructor.name} must be an object`);
+    if (!payload || typeof payload !== "object" || Array.isArray(payload))
+    {
+      throw new TypeError(`Payload data passed to ${this.constructor.name} must be an object`);
+    }
     this.payload = payload instanceof AbstractPayload ? payload : this.getPayload(payload);
     if (!this.payload.validate()) throw new TypeError(`Invalid payload data passed to ${this.constructor.name}`);
   }
-
+  
   protected abstract getPayload(data: PublicProperties<Payload>): Payload;
-
+  
   protected abstract process(payload: Payload): Response;
-
+  
   public execute(): Response
   {
     return this.process(this.payload);
